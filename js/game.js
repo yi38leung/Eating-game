a2d7e2b1f812e8becab02c6efb3ced883aafc485
Cherry Leung <yi38leung@gmail.com>
2023-03-10 08:35:49 +0100

    Last edit

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
-function gameoverSound1() {
+function backgorundMusic() {
   const sound = new Audio("../src/aduio/ES_Gulp - SFX Producer.mp3");
-  sound.volume = 0.3;
+  sound.volume = 0.05;
+  sound.loop = true;
   sound.play();
 }
